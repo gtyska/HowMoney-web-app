f963ae6ac9fd95463e82d211ae5abb2f887d3709
Martyna Grzegorczyk <martyna.grzegorczyk@gmail.com>
2022-04-19 01:17:22 +0200

    navbar updated and page not found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AssetDetailComponent } from './asset-detail/asset-detail.component';
 import { AssetsPageComponent } from './assets-page/assets-page.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -13,8 +14,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'assets', component: AssetsPageComponent },
   {path: 'asset-detail:id', component: AssetDetailComponent},
-  // in other cases redirect to home
-  { path: '**', redirectTo: '' }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
